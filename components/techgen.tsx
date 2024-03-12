31058617b654f2a0e4f9a78998646bf63383d00f
shrita23 <singhshriya131@gmail.com>
2024-03-12 19:51:00 +0530

    yes

--- components/techgen.tsx
+++ components/techgen.tsx
@@ -26,8 +26,8 @@ export default function AICPP() {
                 <div className="px-6 pt-14 lg:px-8">
                     <div className="mx-auto max-w-2xl pt-24 text-center sm:pt-40">
                         <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-                            Technical General{" "}
-                            <span className="text-[#bf9b30]">Navigation</span>{" "}
+                            Technical{" "}
+                            <span className="text-[#bf9b30]">General</span>{" "}
                         </h2>
                         <p className="mt-6 text-lg leading-8 text-gray-300">
                             The DGCA (Directorate General of Civil Aviation)
